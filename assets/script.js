8fa6c6684fe48cf6a47d1ca4e7b9f975655b802f
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-03-06 18:06:05 -0500

    completed project

--- assets/script.js
+++ assets/script.js
@@ -4,18 +4,25 @@ var currentDay = $("#currentDay");
 var currentTime = moment().hour();
 var tasks = [$("#0").text(), $("#1").text(), $("#2").text(), $("#3").text(), $("#4").text(), $("#5").text(), $("#6").text(), $("#7").text(), $("#8").text(), $("#9").text()];
 
+    
+init();
+
+// shows time at the current date at the top of the page
 function headerTime() {
     var today = moment().format("dddd, MMMM Do h:mm a");
     currentDay.text(today);
 };
 
-function loadSavedTasks() {
-    for (var i = 0; i < tasks.length; i++) {
-        var task = JSON.parse(localStorage.getItem("task${i}"));
-        $("#i").val(task);
-    }
-};
+// pulls any saved tasks from local storage into the fields
+$(document).ready(function(){
+    $(".textarea").each(function(){
+        var id = $(this).attr("id");
+        var value = localStorage.getItem(id);
+        $(this).val(value);
+    });
+  });
 
+// shows whether a task is in the past, present, or future
 function colorCoding() {
     for (var i = 0; i < tasks.length; i++) {
         var hour = (moment(tasks[i], "H a"));
@@ -33,65 +40,16 @@ function colorCoding() {
     }
 };
 
-function saveButton(element) {
-    var save = $(element).parent().prev().children().attr("id");
-    var task = $("#"+save).val();
-    localStorage.setItem("${save}", JSON.stringify("${task}"));
-};
+// when save button is clicked, any text entered in field is saved.
+$(".saveBtn").on("click", handleSave)
 
-
-// button
-    // get a reference to all of the save buttons $(".saveBtn")
-    // 01-Ins_jQuery-Elements
-
-    // add a click handler to each save button using .on() method
-    // 03-Ins_Click-Events
-
-    // traverse the DOM to get the values we need to store in localStorage .parent() .sibling()
-    // 07 Dom Traverse
-
-    // setItems in localStorage to save the values
-    // 04
-
-// handle color coding
-    // use moment library to get the current hour
-    // momentsjs.com/docs/#/get-set/hour
-
-    // get a reference to all timeblocks
-
-    // loop through the timeblocks
-    //unit 03
-
-    // get the data-hour value timeblocks[i].data("hour")
-
-    // if, esle - condition we compare the currentHour with timeBlock < (past), === (present), else (future)
-
-    // adding or removing (or both) classes
-
-// get data from local storage and populate the timeblocks
-    // getItem
-
-    // reference to the textarea (id) 
-
-    // .val()
-
-    // approach
-    // -> $("textarea");
-    // -> loop
-    // -> get a reference to the id of parent()
-    // -> .val() = localStorage.getItem(parentID)
-
-
-
-// call the colorCoding function
-    // setInterval -> .15000
-    // unit 04
+function handleSave(event){
+  var value = $(this).parent().siblings(".textarea").val();
+  var key = $(this).parent().siblings(".textarea").attr("id");
+  localStorage.setItem(key, value);
+}
 
 function init() {
-    loadSavedTasks();
     headerTime();
     setInterval (colorCoding(), 1500);
-    saveButton();
-}
-    
-init();
+};
